d8c7902f329510c02261979c9a5cda990cd75323
Daniel Silveira <dansilveira@gmail.com>
2017-08-18 00:18:14 -0300

    Including Business and Service classes. Adding generic constructor to BaseData

--- Business/Accounts/EmployeeBusiness.cs
+++ Business/Accounts/EmployeeBusiness.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Auctus.DataAccess.Accounts;
+using Auctus.DomainObjects.Accounts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Auctus.Business.Accounts
 {
-    public class EmployeeBusiness
+    public class EmployeeBusiness : BaseBusiness<Employee, EmployeeData>
     {
+  
     }
 }
---
+++ Controller/SecurityServices.cs
@@ -0,0 +1,26 @@
+﻿using Auctus.Business.Security;
+using Auctus.DomainObjects.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auctus.Service
+{
+    public class SecurityServices
+    {
+        public User Login(string login, string password)
+        {
+            return new UserBusiness().Login(login, password);
+        }
+
+        public bool ForgotPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool LoginWithFacebook(string fbTokenHash)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
