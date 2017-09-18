9e95aada9ff7a64d4ddfd8da9ddb9e20bd67e140
Thiago Araujo <thiagomvaraujo@gmail.com>
2017-09-17 23:18:08 -0300

    Register fund adjustments

--- Web/wwwroot/js/home.js
+++ Web/wwwroot/js/home.js
@@ -1,15 +1,5 @@
 ﻿$(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
-
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        var $target = $(e.target);
-
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
+    $('[data-toggle="popover"]').popover();
 
     $(".next-step").click(function (e) {
         if ($(this).closest('form').valid()) {
