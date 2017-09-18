953cbbb77a7821ad08d23c21365a9fca246dd7a4
Thiago Araujo <thiagomvaraujo@gmail.com>
2017-09-18 01:21:05 -0300

    Register fund

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -2,14 +2,15 @@
 $(document).ready(function () {
     $.validator.setDefaults({
         highlight: function (element) {
-            $(element).closest('.form-group').addClass('has-error');
+            $(element).closest('.bmd-form-group').addClass('has-error');
         },
         unhighlight: function (element) {
-            $(element).closest('.form-group').removeClass('has-error');
+            $(element).closest('.bmd-form-group').removeClass('has-error');
         },
-        errorElement: 'span',
+        errorElement: 'div',
         errorClass: 'help-block',
         errorPlacement: function (error, element) {
+            $(element).closest('.bmd-form-group').children('.help-block').remove();
             if (element.parent('.input-group').length) {
                 error.insertAfter(element.parent());
             } else {
