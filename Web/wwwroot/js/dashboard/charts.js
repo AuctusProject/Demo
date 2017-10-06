38a6873cd45c2bf2fcb9163df349a6786638933b
Guilherme Gusman <guilherme.gusman@lg7.com.br>
2017-10-05 23:58:35 -0300

    assets variation over time chart

--- Web/wwwroot/js/dashboard/charts.js
+++ Web/wwwroot/js/dashboard/charts.js
@@ -1,21 +1,51 @@
 ﻿
 $(document).ready(function () {
-    dashboardCharts.loadAssetsAllocationChart();
+    dashboardCharts.init();
 });
 
 var dashboardCharts = {
-
-    loadAssetsAllocationChart: function (data) {
+    init: function () {
+        $('input[type=radio][name=chart-options]').change(function () {
+            if (this.id == 'allocation') {
+                $('#allocation-chart').css('display', 'block');
+                $('#variation-chart').css('display', 'none');
+            }
+            else if (this.id == 'variation') {
+                $('#allocation-chart').css('display', 'none');
+                $('#variation-chart').css('display', 'block');
+            }
+        });
+        this.loadAssetsAllocationChart();
+        this.loadAssetsVariationChart();
+    },
+    loadAssetsAllocationChart: function () {
         var assets = pensionFundData.assets;
         var dataArray = [];
         for (var i = 0; i < assets.length; ++i) {
             dataArray.push([assets[i].name, assets[i].percentage]);
         }
         var chart = c3.generate({
-            bindto: '#assets-chart',
+            bindto: '#allocation-chart',
             data: {
                 columns: dataArray,
                 type: 'donut'
+            }
+        });
+    },
+    loadAssetsVariationChart: function () {
+        var assetsReferenceValue = pensionFundData.assetsReferenceValue;
+        var dataArray = [];
+        dataArray.push('Portifolio Variation');
+        for (var i = 0; i < assetsReferenceValue.length; ++i) {
+            dataArray.push(assetsReferenceValue[i].value);
+        }
+        var chart = c3.generate({
+            bindto: '#variation-chart',
+            data: {
+                columns: [dataArray]
+            },
+            color: {
+                pattern: ['#0243b7']
             }
         });
     }
